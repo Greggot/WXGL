824e2dca9f092d5f92e59f8088abf1093e85610f
Greggot <alexgerggot@gmail.com>
2022-09-10 16:43:59 +0300

    Cleanup Core, Fix active passing between models

--- WXGL/Assembly.Core.hpp
+++ WXGL/Assembly.Core.hpp
@@ -15,25 +15,28 @@ namespace Assembly
 	public:
 		Core() {};
 		BaseModel& operator[](size_t i) { return *Models[i]; }
-
-		void append(BaseModel* model) { Models.push_back(model); Active = model; }
-		void remove(uint16_t index) { delete Models[index]; }
-		void clear() { for (auto model : Models) delete model; }
 		size_t size() const { return Models.size(); }
-
-		BaseModel* active() const { return Active; }
-		size_t activeindex() const { return Index; }
-		void setActive(size_t Index) { this->Index = Index; Active = Models[Index]; }
-
 		// Need to iterate throgh by ranged loops
 		std::vector<BaseModel*>::iterator begin() { return Models.begin(); }
 		std::vector<BaseModel*>::iterator end() { return Models.end(); }
 
+		void append(BaseModel* model) { 
+			Models.push_back(model); 
+			 setActive(size() - 1);
+		}
+		void remove(uint16_t index) { delete Models[index]; }
+		void clear() { for (auto model : Models) delete model; }
+
+		BaseModel* active() const { return Active; }
+		size_t activeindex() const { return Index; }
+		void setActive(size_t Index) { 
+			this->Index = Index; 
+			if(Active)
+				Active->Active = false;
+			Active = Models[Index]; 
+			Active->Active = true;
+		}
+
 		~Core() { clear(); }
 	};
 }
-//
-//void something()
-//{
-//	Assembly::Core core;
-//}
